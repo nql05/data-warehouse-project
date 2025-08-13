42461c1b54f762b76e388e3c31bfbeb0b64062c3
Nguyễn Quang Linh <144930308+nql05@users.noreply.github.com>
2025-08-13 09:16:27 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,7 @@
+--Create Database for our Data Warehouse--
+create database DataWarehouse;
 
+--Create Schema for each layer that we are going to design--
+create schema bronze;
+create schema silver;
+create schema gold;
